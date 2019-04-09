18f1d849494d42ff6b3dd10ebe555a127f6ced71
Daenise <daenise@easystore.co>
2019-04-10 01:39:04 +0800

    setAppoinment css done

--- resources/views/appointment/setAppointment.blade.php
+++ resources/views/appointment/setAppointment.blade.php
@@ -22,105 +22,76 @@
 				<div class="container">
 					<div class="row">
 						<div class="booking-form">
+
 							<div class="booking-bg">
 								<div class="form-header">
 									<h2>Set Appointment</h2>
-										<span class="fa fa-calendar calendarIcon"></span>
+									<span class="fa fa-calendar calendarIcon"></span>
 								</div>
 							</div>
-							<form>
+
+							<form method="POST" action="/saveAppointment">
+								{{csrf_field()}}
+
 								<div class="row">
-								<div class="col-md-6">
-									<div class="form-group">
-										<span class="form-label">My Item</span>
-										<select class="form-control">
-												<option>Item 1</option>
-												<option>Item 2</option>
-												<option>Item 3</option>
+									<div class="col-md-6">
+										<div class="form-group">
+											<span class="form-label">My item to trade</span>
+											<select class="form-control" name="buyer_item_id">
+												@foreach($itemList as $i)
+												<option value="{{$i->id}}">{{$i->item_title}}</option>
+												@endforeach
 											</select>
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="form-group">
-										<span class="form-label">Items that you want</span>
-										<select class="form-control">
-												<option>Item 1</option>
-												<option>Item 2</option>
-												<option>Item 3</option>
-											</select>
-									</div>
-								</div>
-							</div>
-
-									<form method="POST" action="/saveAppointment">
-                                        {{csrf_field()}}
-										<h1>Owner ID : {{$dataList['owner']}}</h1>
-										<h1>Buyer ID : {{$dataList['buyer']}}</h1>
-										<h1>Item ID : {{$dataList['itemID']}}</h1>
-										<div class="row">
-											<div class="col-md-6">
-												<div class="form-group">
-													<span class="form-label">My item to trade</span>
-													<select class="form-control" name="buyer_item_id">
-														@foreach($itemList as $i)
-                                                            <option value="{{$i->id}}">{{$i->item_title}}</option>
-                                                        @endforeach
-													</select>
-													<span class="select-arrow"></span>
-												</div>
-											</div>
-											<div class="col-md-6">
-												<div class="form-group">
-													<span class="form-label">Item that want to trade</span>
-                                                    <input type="text" value="{{$item->item_title}}" readonly>
-                                                    <input type="text" name="itemID" value="{{$item->id}}">
-                                                    <input type="text" name="ownerID" value="{{$item->owner}}">
-													<span class="select-arrow"></span>
-												</div>
-											</div>
+											<span class="select-arrow"></span>
 										</div>
-
-										<div class="row">
-											<div class="col-md-6">
-												<div class="form-group">
-													<span class="form-label">Date</span>
-													<input class="form-control" type="date" name="date" required>
-												</div>
-											</div>
-											<div class="col-md-6">
-												<div class="form-group">
-													<span class="form-label">Time</span>
-													<input class="form-control" type="time" name="time" required>
-												</div>
-											</div>
-										</div>
-
-										<div class="row">
-											<div class="col-md-6">
-												<div class="form-group">
-													<span class="form-label">Location</span>
-													<input class="form-control" type="text" name="location" required>
-												</div>
-											</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<span class="form-label">Item To Trade</span>
+											<input type="text" class="form-control" value="{{$item->item_title}}" readonly>
+											<input type="text" name="itemID" style="visibility: hidden;" value="{{$item->id}}">
+											<input type="text" name="ownerID" style="visibility: hidden;" value="{{$item->owner}}">
+											<span class="select-arrow"></span>
 										</div>
 									</div>
 								</div>
 
-										<div class="form-btn">
-											<button class="submit-btn" type="submit">Save</button>
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<span class="form-label">Date</span>
+											<input class="form-control" type="date" name="date" required>
 										</div>
-									</form>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<span class="form-label">Time</span>
+											<input class="form-control" type="time" name="time" required>
+										</div>
+									</div>
 								</div>
+
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<span class="form-label">Location</span>
+											<input class="form-control" type="text" name="location" required>
+										</div>
+									</div>
+								</div>
+
+
+								<div class="form-btn">
+									<button class="submit-btn" type="submit">Save</button>
+								</div>
+
 							</form>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</body>
-
-	</html>
-</form>
-</div>
-
+	</div>
 </body>
+
+</html>
