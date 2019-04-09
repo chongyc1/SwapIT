fe55330175f312eaf0d1a4695963a7919ee7f28d
Daenise <daenise@easystore.co>
2019-04-09 16:31:38 +0800

    added appointment

---
+++ resources/views/appointment/setAppointment.blade.php
@@ -0,0 +1,85 @@
+<!--setAppoinment.html-->
+
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<div>
+		<form>
+			<body>
+				<div id="booking" class="section">
+					<div class="section-center">
+						<div class="container">
+							<div class="row">
+								<div class="booking-form">
+									<div class="booking-bg">
+										<div class="form-header">
+											<h2>Set Appoinment to Trade</h2>
+										</div>
+									</div>
+									<form>
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<span class="form-label">Date</span>
+													<input class="form-control" type="date" required>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="form-group">
+													<span class="form-label">Time</span>
+													<input class="form-control" type="time" required>
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<span class="form-label">Adults</span>
+													<select class="form-control">
+														<option>1</option>
+														<option>2</option>
+														<option>3</option>
+													</select>
+													<span class="select-arrow"></span>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="form-group">
+													<span class="form-label">Children</span>
+													<select class="form-control">
+														<option>0</option>
+														<option>1</option>
+														<option>2</option>
+													</select>
+													<span class="select-arrow"></span>
+												</div>
+											</div>
+										</div>
+										<div class="form-group">
+											<span class="form-label">Room Type</span>
+											<select class="form-control" required>
+												<option value="" selected hidden>Select room type</option>
+												<option>Private Room (1 to 2 People)</option>
+												<option>Family Room (1 to 4 People)</option>
+											</select>
+											<span class="select-arrow"></span>
+										</div>
+										<div class="form-btn">
+											<button class="submit-btn">Check availability</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</body>
+
+			</html>
+		</form>
+	</div>
+
+</body>
