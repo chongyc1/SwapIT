9f450d4b6ab4c5e3d7a9db3401cdb90842492489
Yc <>
2019-04-09 23:24:30 +0800

    update appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -9,4 +9,29 @@ class AppointmentController extends Controller
     public function index(){
     	return view('appointment.setAppointment');
     }
+
+    public function setAppointment(Request $request){
+
+//        $request->validate([
+//            'location' => 'required',
+//            'date' => 'required|date|after:today',
+//
+//        ]);
+
+
+        DB::table('appointments')->insert([
+            'owner' => '',
+            'buyer' => '',
+            'owner_item' => '',
+            'buyer_item' => '',
+            'location' => '',
+            'status' => 'PENDING',
+            'date' => '',
+            'time' => '',
+        ]);
+
+        dd('DONE');
+    }
+
+    public function upd
 }
